21a08d44b9cfde7673cc05580f77d6f3a44a8a03
Collins Kimani <mwas9446.ck@gmail.com>
2024-01-02 07:38:54 -0800

    add files 1-9

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object_oriented programming\
+        language that combines remarkable power with every syntax"
+str = str.replace(str, "object-oriented programming with Python")
+print(str)
---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,2 @@
+#/!usr/bin/python3
+echo "import this" | python3
