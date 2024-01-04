90bd004aeed73592b5edbb94baf0d59afa804b29
Collins Kimani <mwas9446.ck@gmail.com>
2024-01-04 07:58:53 -0800

    update file 2 print messages

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,9 +8,11 @@ if __name__ == "__main__":
     # length = len(sys.argv) - 1
 
     if length == 0:
-        print("{} arguements.".format(length))
+        print("0 arguments.")
+    elif length == 1:
+        print("1 argument:\n1: {}".format(sys.argv[1]))
     else:
-        print("{} arguements:".format(length))
+        print("{} arguments:".format(length))
         # loop the sys argv attribute to print the elements
         for arg in range(1, length + 1):
-            print("{} : {}".format(arg, sys.argv[arg]))
+            print("{}: {}".format(arg, sys.argv[arg]))
