f1c85711182252901b46a8820f70e89a7bde3f26
Collins Kimani <mwas9446.ck@gmail.com>
2024-01-04 04:55:01 -0800

    add files 0, 1 and readme

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from add_0 import add
+
+# assign numbers to variables
+a = 1
+b = 2
+# print the sum with add funtion
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add(a, b)))
---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+from calculator_1 import add, sub, mul, div
+
+# initialize arguements
+a = 10
+b = 5
+
+# call functions with calculation operations
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
