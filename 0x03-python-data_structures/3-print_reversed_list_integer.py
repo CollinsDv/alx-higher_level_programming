2ea35f31fb93dfa872d9af030148501782c754b3
Collins Kimani <mwas9446.ck@gmail.com>
2024-01-06 05:32:34 -0800

    add files 1-4

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+def print_reversed_list_integer(my_list=[]):
+    # reverse the list
+    my_list.reverse()
+
+    # display the items line by line
+    for item in my_list:
+        print("{:d}".format(item))
