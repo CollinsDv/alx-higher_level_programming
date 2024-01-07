065ce06b0b3b5ec634d4994ad9776bf5404782a0
Collins Kimani <mwas9446.ck@gmail.com>
2024-01-07 18:28:33 +0300

    update file 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_b) == 0:
-        return tuple_a
-    elif len(tuple_a) < 2:
+    if not tuple_a and not tuple_b:
+        tuple_a = tuple(0, 0)
+        tuple_b = tuple(0, 0)
+    if len(tuple_a) < 2 and len(tuple_b) < 2 : 
         tuple_a = (tuple_a[0], 0)
-    elif len(tuple_b) < 2:
         tuple_b = (tuple_b[0], 0)
     
     new_list = [] 
