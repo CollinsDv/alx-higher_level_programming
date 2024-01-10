278b08b0d7570e670023efbafa55856e641453d5
Collins Kimani <mwas9446.ck@gmail.com>
2024-01-11 00:15:53 +0300

    used max function

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    '''
     if a_dictionary is None:
         return
     # sort the dict in reverse order
     tup = sorted(a_dictionary.items(), reverse=True)
     #change 1st tuple element to list and obtain the name
-    return list(tup[0])[0]
+    if list(tup[0])[0] == 0
+    '''
+    if not a_dictionary:
+        return None
+
+    return max(a_dictionary, key=a_dictionary.get)
