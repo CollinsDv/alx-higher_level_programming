1bbcae6b63ac30201eff2f77e99964fa28446cb5
Collins Kimani <mwas9446.ck@gmail.com>
2024-01-10 18:46:34 +0300

    add file 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    # remove duplicates
+    new_set = set(my_list)
+    
+    total = 0
+    # loop set to access items
+    for num in new_set:
+        total += num
+        
+    return total
