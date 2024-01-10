de5d34b93a58d20a7ac3bb74c8caa05261d2e38e
Collins Kimani <mwas9446.ck@gmail.com>
2024-01-10 17:25:52 +0300

    handle pycodestyle error

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    mult = lambda x : x * x
 
-    return [[mult(num) for num in row] for row in matrix]
+    return [[num*num for num in row] for row in matrix]
