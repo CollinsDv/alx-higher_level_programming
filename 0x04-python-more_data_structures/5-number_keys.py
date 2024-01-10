b3cd7689784d846800ba3bce8e091d9ef89126a5
Collins Kimani <mwas9446.ck@gmail.com>
2024-01-10 19:21:23 +0300

    add file 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    key_count = 0
+    # loops the dict keys to tally them
+    for key in a_dictionary:
+        key_count += 1
+
+    return key_count
