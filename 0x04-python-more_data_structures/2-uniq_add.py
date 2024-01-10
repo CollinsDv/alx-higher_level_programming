79cb29d060504667cd5a6d1af7933cb5ae696396
Collins Kimani <mwas9446.ck@gmail.com>
2024-01-10 18:48:02 +0300

    removed whitespace

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,10 +2,10 @@
 def uniq_add(my_list=[]):
     # remove duplicates
     new_set = set(my_list)
-    
+
     total = 0
     # loop set to access items
     for num in new_set:
         total += num
-        
+
     return total
