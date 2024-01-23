5606feb213ad3f332ce23efa7a23d1014c9f459d
Collins Kimani <mwas9446.ck@gmail.com>
2024-01-23 18:54:15 +0300

    add file 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Filename: 1-square"""
+
+
+class Square:
+    """An class that defines a square without private attribute:"""
+    def __init__(self, size):
+        self.__size = size
