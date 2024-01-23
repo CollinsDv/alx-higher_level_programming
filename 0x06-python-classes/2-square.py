2e881d8321e4d35564232833816d634153acd9ae
Collins Kimani <mwas9446.ck@gmail.com>
2024-01-23 19:28:42 +0300

    handle ValueError

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,7 +8,7 @@ class Square:
     def __init__(self, size=0):
         try:
             size = int(size)
-        except TypeError:
+        except ValueError:
             raise TypeError("size must be an integer")
         else:
             if size < 0:
