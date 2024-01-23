b5f4c22c6456f559d67feee2224873db045bfdbf
Collins Kimani <mwas9446.ck@gmail.com>
2024-01-23 20:16:58 +0300

    correct return types for the set decorators

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -25,10 +25,10 @@ class Square:
         if type(value) is not int:
             raise TypeError("size must be an integer")
         else:
-            if size < 0:
+            if value < 0:
                 raise ValueError("size must be >= 0")
             else:
-                self.__size = size
+                self.__size = value
 
     def area(self):
         return self.__size * self.__size
