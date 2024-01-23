9199ee31bb1fdf50ff365e4404436d515f8bd653
Collins Kimani <mwas9446.ck@gmail.com>
2024-01-23 23:31:59 +0300

    handled whitespace

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,6 @@
 class Square:
     """An class that defines a square without private attribute:"""
     def __init__(self, size=0):
-        
         if type(size) is not int:
             raise TypeError("size must be an integer")
         else:
