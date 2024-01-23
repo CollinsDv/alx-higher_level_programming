c2e9bc62dd22a70ecf90509ad95eaab463ed0816
Collins Kimani <mwas9446.ck@gmail.com>
2024-01-23 23:28:55 +0300

    update file 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,63 +1,60 @@
 #!/usr/bin/python3
-
-"""Filename: 3-square.py"""
+"""Coordinates of a square"""
 
 
 class Square:
-	"""An class that defines a square with private attribute
-	and returns the area
-	Also handles use of setters and getters
-	"""
-	def __init__(self, size=0, position=(0, 0)):
-		if type(size) is not int:
-			raise TypeError("size must be an integer")
-		else:
-			if size < 0:
-				raise ValueError("size must be >= 0")
-			else:
-				self.__size = size
+    """Private instance attribute: size
+    Instantiation with area and position method """
 
-	@property
-	def size(self):
-		return self.__size
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes attribute size """
+        self.size = size
+        self.position = position
 
-	@property
-	def position(self):
-		return self.__position
+    def area(self):
+        """Calculate area of square"""
+        return (self.__size * self.__size)
 
-	@size.setter
-	def size(self, value):
-		if type(value) is not int:
-			raise TypeError("size must be an integer")
-		else:
-			if value < 0:
-				raise ValueError("size must be >= 0")
-			else:
-				self.__size = value
+    @property
+    def size(self):
+        """Getter for square"""
+        return self.__size
 
-	@position.setter
-	def position(self, value):
-		if type(value) is not tuple or len(value) != 2 or type(value[0]) is not int
-		and type(value[1]) is not int or value[0] < 0 or value[1] < 0:
-			raise TypeError("position must be a tuple of 2 positive
-							 integers")
-		else:
-			self.__position = value
+    @size.setter
+    def size(self, value):
+        """Initializes attribute size """
+        if (type(value) is not int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-	def area(self):
-		return self.__size * self.__size
+    @property
+    def position(self):
+        """Getter for position"""
+        return self.__position
 
-	def my_print(self):
-		def my_print(self):
-			"""Print method"""
-		if (self.size == 0):
-			print()
-		else:
-			for i in range(self.position[1]):
-				print()
-			for j in range(0, self.size):
-				for e in range(self.position[0]):
-					print(" ", end="")
-				for j in range(self.size):
-					print("#", end="")
-				print() 
+    @position.setter
+    def position(self, value):
+        """Initializes attribute position"""
+        if len(value) is not 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if (type(value[0]) is not int or value[0] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if (type(value[1]) is not int or value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        """Print method"""
+        if (self.size == 0):
+            print()
+        else:
+            for i in range(self.position[1]):
+                print()
+            for j in range(0, self.size):
+                for e in range(self.position[0]):
+                    print(" ", end="")
+                for j in range(self.size):
+                    print("#", end="")
+                print()
