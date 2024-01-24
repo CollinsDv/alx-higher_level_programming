31fc3fad42d8b622b49321601eb89e00ea28e97f
Collins Kimani <mwas9446.ck@gmail.com>
2024-01-24 14:52:31 +0300

    handled pycodestyle errors

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except (ValueError, TypeError):
-        print("Exception: Unknown format code 'd' for object of type 'str'", file=sys.stderr)
+        print("Exception: Unknown format code 'd' for object of type 'str'",
+              file=sys.stderr)
         return False
     else:
         return True
