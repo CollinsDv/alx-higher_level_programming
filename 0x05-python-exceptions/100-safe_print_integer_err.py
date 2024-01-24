e16c02a975e8018103d4169ebcb1e3929751114d
Collins Kimani <mwas9446.ck@gmail.com>
2024-01-24 15:05:31 +0300

    handle proper exception handling

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,9 +5,8 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except (ValueError, TypeError):
-        print("Exception: Unknown format code 'd' for object of type 'str'",
-              file=sys.stderr)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
         return False
     else:
         return True
