2c63a9af77f7475fb3a4a7aaba39b6e2b31e259b
Collins Kimani <mwas9446.ck@gmail.com>
2024-02-05 23:25:09 +0300

    handle PEP8

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,9 +5,11 @@ Write a class MyInt that inherits from int
 
 
 class MyInt(int):
-	"""assign __eq__ to __ne__ and vice versa"""
+    """assign __eq__ to __ne__ and vice versa"""
     def __eq__(self, value):
+        """new __ne__"""
         return int(self) != int(value)
 
     def __ne__(self, value):
+        """new __eq__"""
         return int(self) == int(value)
