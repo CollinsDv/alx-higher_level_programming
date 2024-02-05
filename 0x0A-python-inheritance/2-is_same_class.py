a1d024aa3c0a747cb64259c1f7057e7585ac01ea
Collins Kimani <mwas9446.ck@gmail.com>
2024-02-05 17:19:08 +0300

    add file 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+module determining class instances
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Return True if obj is instance of a class,
+    otherwise False
+
+    Args:
+        obj - Object of interest
+        a_class - aledged inherited class
+
+    """
+    if isinstance(a_class, obj) is True:
+        return True
+    else:
+        return False
