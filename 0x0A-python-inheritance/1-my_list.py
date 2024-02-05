05c6bc88f462ff220dcfba3b0da0edabc7e3ce44
Collins Kimani <mwas9446.ck@gmail.com>
2024-02-05 16:22:24 +0300

    add file 1 and its test file

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+"""1-my_list
+"""
+
+
+class MyList(list):
+    """
+    inherits capabilities of populating a list and prints
+    it
+    """
+    def print_sorted(self):
+        """print a list in sorted order"""
+        print(sorted(self))
