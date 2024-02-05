d7b729474f57ebcea07276a2eefa5386268d66a4
Collins Kimani <mwas9446.ck@gmail.com>
2024-02-05 17:49:30 +0300

    add file 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+module determining types and class instances
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Return True if obj is an instance of a class that inherited
+    from a specified class, otherwise False
+
+    Args:
+        obj - Object of interest
+        a_class - aledged inherited class
+
+    """
+    if issubclass(obj, a_class):
+        return True
+
+    return False
