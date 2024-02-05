269fa70bf9394f5af06e156c29a563a2dd26f6be
Collins Kimani <mwas9446.ck@gmail.com>
2024-02-05 22:43:09 +0300

    add file 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""file: 8-base_geometry.py
+"""
+
+
+class BaseGeometry():
+    """handles inheritance"""
+    def area(self):
+        """raise an error"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validate parameter values
+
+            Args:
+                name (str): string parameter
+                value (int): integer parameter
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """handle checksum of values without
+    getters and setters
+    """
+    def __init__(self, width, height):
+        """act as getter and setter"""
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """compute area"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
