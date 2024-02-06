af3a10ac1cb1164a9e499e21d45acf2e3691895d
Collins Kimani <mwas9446.ck@gmail.com>
2024-02-06 22:49:15 +0300

    update file 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,14 +6,14 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = 'file.json'
-List = []
+argc = len(sys.argv)
 
+filename = 'add_item.json'
+my_list = []
 try:
-    List = load_from_json_file(filename)
+    my_list = load_from_json_file(filename)
 except BaseException:
     pass
-
-List.extend(sys.argv[1:])
-
-save_to_json_file(List, filename)
+for items in range(1, argc):
+    my_list.append(argv[items])
+save_to_json_file(my_list, filename)
