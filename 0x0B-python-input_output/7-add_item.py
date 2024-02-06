1ec9e6f92ae31738390bf7f6b3e2d173268b7ba2
Collins Kimani <mwas9446.ck@gmail.com>
2024-02-06 22:57:22 +0300

    update file 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,6 +2,7 @@
 """adds arguements to a Python list then
 saves it to a file
 """
+from os import path
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
