53aae8c12b5593133c70db3ce1b88937ab6e6df8
Collins Kimani <mwas9446.ck@gmail.com>
2024-03-12 22:06:01 +0300

    update number 1

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,8 +1,2 @@
--- creates a database and restricts output if it already exists
-IF NOT EXISTS (
-	SELECT name FROM master.sys.databases
-	WHERE name = N'hbtn_0c_0'
-)
-BEGIN
-	CREATE DATABASE hbtn_0c_0;
-END;
+-- creates a database and restricts output if it already exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
