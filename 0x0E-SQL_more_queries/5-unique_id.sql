c53d947805bddb6bb8bfdc6fc4076328962f16ac
Collins Kimani <mwas9446.ck@gmail.com>
2024-03-13 19:40:17 +0300

    update database name to create

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,7 +1,7 @@
 -- creates a table in a database and shouldn't raise an error
 -- if it exists. Database will be passed as cmd arguement
 -- Id field has a default value and should be uniquee
-CREATE TABLE IF NOT EXISTS id_not_null (
+CREATE TABLE IF NOT EXISTS unique_id (
 	id INT DEFAULT 1 UNIQUE,
 	name VARCHAR(256)
 );
