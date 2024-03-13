edbf2616b78877cef41a20db06080b3397076fa5
Collins Kimani <mwas9446.ck@gmail.com>
2024-03-13 19:58:21 +0300

    add use command

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -3,6 +3,8 @@
 -- 	id is unique, not null and auto-generete, name should not be NULL
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+USE hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS states (
 	id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
