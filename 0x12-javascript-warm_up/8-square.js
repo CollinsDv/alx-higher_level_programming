13478d0231d6864d756e6176eb96b0d4abb70d43
Collins Kimani <mwas9446.ck@gmail.com>
2024-04-09 01:43:49 +0300

    add file 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// prints a square if argv[2] is not NaN
+const width = Number(process.argv[2]);
+if (isNaN(width)) {
+  console.log('Missing size');
+}
+else {
+  for (let i = 0; i < width; i++) {
+    for (let j = 0; j < width; j++) {
+      process.stdout.write('X');
+      if (j === width - 1) { process.stdout.write('\n') }
+    }
+  }
+}
