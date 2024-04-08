b639ee8d3057e104cc8b56af656ae81eb5b77187
Collins Kimani <mwas9446.ck@gmail.com>
2024-04-09 01:48:34 +0300

    handled semistandards

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,12 +3,11 @@
 const width = Number(process.argv[2]);
 if (isNaN(width)) {
   console.log('Missing size');
-}
-else {
+} else {
   for (let i = 0; i < width; i++) {
     for (let j = 0; j < width; j++) {
       process.stdout.write('X');
-      if (j === width - 1) { process.stdout.write('\n') }
+      if (j === width - 1) { process.stdout.write('\n'); }
     }
   }
 }
