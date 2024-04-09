9ab12f9430f5929becc15fe71145edcb52d35b08
Collins Kimani <mwas9446.ck@gmail.com>
2024-04-09 22:39:29 +0300

    remove property declarations

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 // create a class. If w or h is < 0, create an empty class
 module.exports = class Rectangle {
-  width;
-  height;
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
