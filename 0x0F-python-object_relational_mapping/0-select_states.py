8423aff6b56dfd141397830564ad3cc195a73810
Collins Kimani <mwas9446.ck@gmail.com>
2024-04-11 19:48:38 +0300

    ensure code executes as main

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
+"""accessing and printing a database contents
 """
-accessing and printing a database contents
-"""
-import MySQLdb
-import sys
+def main():
+    """executes the code below"""
+    # connect to database
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-# connect to database
-db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    # cursor
+    cur = db.cursor()
 
-# cursor
-cur = db.cursor()
+    # execute query
+    cur.execute('SELECT * FROM states ORDER BY id ASC')
 
-# execute query
-cur.execute('SELECT * FROM states ORDER BY id ASC')
+    # obtain results
+    rows = cur.fetchall()
 
-# obtain results
-rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
-for row in rows:
-    print(row)
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    main()
