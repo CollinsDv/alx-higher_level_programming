ed3a5ac0606a43c6252a0f9bd529a0ea4b15156e
Collins Kimani <mwas9446.ck@gmail.com>
2024-04-13 17:26:32 +0300

    handle pep on model_city

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -9,7 +9,7 @@ class City(Base):
     """defining cities class to be mapped to table"""
     __tablename__ = 'cities'
 
-    id = Column(autoincrement = True, Integer,
-            nullable = False, primary_key = True)
-    name = Column(String(128), nullable = False)
-    state_id = Column(Integer, nullable = False, ForeignKey('states.id'))
+    id = Column(autoincrement=True, Integer,
+                nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False, ForeignKey('states.id'))
