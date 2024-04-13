26fa46951eea76eeccf748d76b2414f01be6ac3d
Collins Kimani <mwas9446.ck@gmail.com>
2024-04-13 07:28:47 +0300

    add file 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""generate query to database
+"""
+
+from sqlalchemy import create_engine
+from model_state import Base, State
+import sys
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
+    # initialize a session to communicate with database
+    session = Session()
+
+    result = session.query(State).order_by(id)
+
+    for row in result:
+        print(row)
