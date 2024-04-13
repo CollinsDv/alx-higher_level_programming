02ee3c42ed11cc092c87a12f7df6319127e86abc
Collins Kimani <mwas9446.ck@gmail.com>
2024-04-14 02:12:18 +0300

    handle error handling and clean up

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,21 +5,32 @@
 
 def main():
     """prints ensuring certain query requirements"""
-    # connect to database
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    try:
+        # connect to database
+        db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-    # cursor
-    cur = db.cursor()
 
-    # execute query
-    cur.execute('SELECT * FROM states WHERE name like "N%" ORDER BY id ASC')
+        cur = db.cursor()
 
-    # obtain results
-    rows = cur.fetchall()
+        # execute query
+        cur.execute('SELECT * FROM states WHERE name like "N%" ORDER BY id ASC')
 
-    if rows:
-        for row in rows:
-            print(row)
+        # obtain results
+        rows = cur.fetchall()
+
+        if rows:
+            for row in rows:
+                print(row)
+        
+        # clean up
+        cur.close()
+        db.close()
+
+    except MySQLdb.Error, e:
+        pass
+    finally:
+        if 'db' in locals():
+            db.close()
 
 
 if __name__ == '__main__':
