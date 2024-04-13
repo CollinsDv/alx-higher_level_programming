a6be40cf6a904958b393c3d2ff8cbcd1e304a0f6
Collins Kimani <mwas9446.ck@gmail.com>
2024-04-13 17:10:19 +0300

    add docstring

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """base class definition for city"""
 
-from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 
-Base = declarative_base()
 
 class City(Base):
+    """defining cities class to be mapped to table"""
     __tablename__ = 'cities'
 
-    id = Column(autoincrement = True, Integer, unique = True,
+    id = Column(autoincrement = True, Integer,
             nullable = False, primary_key = True)
     name = Column(String(128), nullable = False)
     state_id = Column(Integer, nullable = False, ForeignKey('states.id'))
