7c61e3cf6e41cded97e491a00486ef76a9541ca3
Collins Kimani <mwas9446.ck@gmail.com>
2024-04-14 02:19:49 +0300

    handle errors

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,13 +7,14 @@ def main():
     """prints ensuring certain query requirements"""
     try:
         # connect to database
-        db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-
+        db = MySQLdb.connect(
+                user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
         cur = db.cursor()
 
         # execute query
-        cur.execute('SELECT * FROM states WHERE name like "N%" ORDER BY id ASC')
+        cur.execute('''SELECT * FROM states
+                    WHERE name like "N%" ORDER BY id ASC''')
 
         # obtain results
         rows = cur.fetchall()
@@ -21,12 +22,8 @@ def main():
         if rows:
             for row in rows:
                 print(row)
-        
-        # clean up
-        cur.close()
-        db.close()
 
-    except MySQLdb.Error, e:
+    except MySQLdb.Error as e:
         pass
     finally:
         if 'db' in locals():
