b11448a01f013fdc2153ba34acf3bb3e762e9cdb
Collins Kimani <mwas9446.ck@gmail.com>
2024-04-13 17:23:04 +0300

    update entire code

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -2,20 +2,21 @@
 """prints contents of a database joined to another
 """
 
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+import sys
 from model_state import State, Base
 from model_city import City
-import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-
+if __name__ == "__main__":
+    """
+    Addess to database and retrieve states from database
+    """
+    db = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine(db)
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
-
     session = Session()
 
     for state, city in session.query(State, City).filter(
