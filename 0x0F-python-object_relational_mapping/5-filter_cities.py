bc66a6aeb9018ef97f96c1c848a65045b7ff804f
Collins Kimani <mwas9446.ck@gmail.com>
2024-04-14 04:38:47 +0300

    reformat

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python3
 """takes in the name of a state as an argument and lists all cities
 of that state, using the database hbtn_0e_4_usa"""
+import MySQLdb
+from sys import argv
 
-if __name__ == '__main__':
-    import sys
-    import MySQLdb
+if __name__ == "__main__":
 
-    # connect
-    db = MySQLdb.connect(user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argvs
+    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                           user=argv[1], passwd=argv[2], db=argv[3])
 
-	cur = db.cursor()
-
-    cur.execute("""SELECT cities.id, cities.name
-        FROM cities
-        JOIN states ON cities.state_id = states.id
-        WHERE states.name LIKE BINARY %s
-        ORDER BY cities.id ASC
-    	""", (sys.argv[4],))
-
-    rows = cur.fetchall()
-
-    if rows is not None:
-        print(", ".join([row[1] for row in rows]))
-
+    cur = conn.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name\
+            FROM cities LEFT JOIN\
+            states ON cities.state_id = states.id WHERE states.name=%s\
+            ORDER by cities.id ASC;",
+                (argv[4], ))
+    query_rows = cur.fetchall()
+    print(', '.join([row[1] for row in query_rows]))
     cur.close()
-    db.close()
+    conn.close()
