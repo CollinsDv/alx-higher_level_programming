ceefa99d43f4915dd98ca9d91e839c9c08ecc618
collins Kimani <mwas9446.ck@gmail.com>
2024-04-26 22:43:11 +0300

    add file 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""module: 1-hbtn_header
+    takes in a URL, sends a request to the URL and displays the value of    the X-Request-Id variable found in the header of the response.
+        - use only urllib and sys
+        - value of variable varies with each request
+        - You don't need to check the number/type of args passed to script
+        - must use with
+"""
+import urllib.request
+import sys
+
+# generate request object
+request_obj = urllib.request.Request(sys.argv[1])
+# use context manager to open a link
+with urllib.request.urlopen(request_obj) as response:
+    value = response.getheader('X-Request-Id')
+    print(value)
