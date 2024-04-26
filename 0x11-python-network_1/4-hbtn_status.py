50f27f79c94c21989b8b0010caffccbd7c18d164
collins Kimani <mwas9446.ck@gmail.com>
2024-04-27 00:47:58 +0300

    add file 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""module: 4-hbtn_status
+    fetches https://alx-intranet.hbtn.io/status
+        - You must use the package requests
+        - You are not allow to import packages other than requests
+        - The body of the response must be display like the following example
+        (tabulation before -)
+        guillaume@ubuntu:~/0x11$ ./4-hbtn_status.py | cat -e
+        Body response:$
+            - type: <class 'str'>$
+            - content: OK$
+"""
+import requests
+if __name__ == '__main__':
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print('\ttype:', type(req.text))
+    print('\tcontent', req.text)
