a7d6c3cfbec14664e0e0225dc8f7db2c0f280fd3
collins Kimani <mwas9446.ck@gmail.com>
2024-04-27 01:37:18 +0300

    add file 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""module: 5-hbtn_header
+    takes in a URL, sends a request to the URL and displays
+    the value of the variable X-Request-Id in the response header
+        - You must use the packages requests and sys
+        - You are not allow to import other packages than requests and sys
+        - The value of this variable is different for each request
+        - You don’t need to check script arguments (number and type)
+"""
+import requests
+if __name__ == '__main__':
+    response = requests.head('https://alx-intranet.hbtn.io/status')
+    print(response.headers["X-Request-Id"])
