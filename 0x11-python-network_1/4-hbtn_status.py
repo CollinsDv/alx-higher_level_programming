6f783307a72cee774c0b68fbf4afa1e42b89d6da
collins Kimani <mwas9446.ck@gmail.com>
2024-04-27 01:29:19 +0300

    add __name__ dunder check

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -11,8 +11,8 @@
             - content: OK$
 """
 import requests
-# if __name__ == '__main__':
-response = requests.get('https://alx-intranet.hbtn.io/status')
-print('Body response:')
-print('\t- type:', type(response.text))
-print('\t- content', response.text)
+if __name__ == '__main__':
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type:', type(response.text))
+    print('\t- content', response.text)
