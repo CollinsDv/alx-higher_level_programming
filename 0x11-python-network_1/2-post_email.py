95f1321e7c4c2788f22568b25b873c34c748e47c
collins Kimani <mwas9446.ck@gmail.com>
2024-04-26 23:55:48 +0300

    handle PEP coding style

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """module: 2-post_email.py
-    takes in a URL and an email, sends a POST request to the passed URL with the email
-    as a parameter, and displays the body of the response (decoded in utf-8)
+    takes in a URL and an email, sends a POST request to the passed URL
+    with the email as a parameter, and displays the body of the response
+    (decoded in utf-8)
         - The email must be sent in the email variable
         - You must use the packages urllib and sys
         - You are not allowed to import packages other than urllib and sys
-        - You don’t need to check arguments passed to the script (number or type)
+        - You don’t need to check arguments passed to the
+          script (number or type)
         - You must use the with statement
 """
 import urllib.request
