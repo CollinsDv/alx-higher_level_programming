1b96ecc6d87670c453a75f7a17320cfadcd19231
Collins Kimani <mwas9446.ck@gmail.com>
2024-05-22 20:59:00 +0300

    fine refinement

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,29 +1,12 @@
 #!/usr/bin/node
-/*
-gets webpage contents and stores in a file
-- 1st arg: URL request
-- 2nd arg: file path (UTF-8 encoded)
-- use request module
-*/
 
 const request = require('request');
 const fs = require('fs');
 
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-  if (response.statusCode !== 200) {
-    console.error('Error: Failed to fetch data. Status code:', response.statusCode);
-    return;
-  }
-  fs.writeFile(filePath, body, 'utf-8', (err) => {
+request(process.argv[2], (_err, _res, body) => {
+  fs.writeFile(process.argv[3], body, 'utf8', (err) => {
     if (err) {
-      console.error('Error writing to file:', err);
+      console.log(err);
     }
   });
 });
